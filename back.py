baf6d8aae2508dc4e9869fe01f35d60848319e5c
FatmaAtta <fatmamaali@gmail.com>
2025-05-06 12:18:11 +0300

    cleaning up back chaining

--- back.py
+++ back.py
@@ -3,7 +3,6 @@ def read_facts(file):
     file = open(file,'r')
     for line in file:
         facts.append(line.strip().replace('skin_smell','perfumed'))
-    # processed_facts = []
     processed_fact = {}
     for fact in facts:
         if '=' in fact:
@@ -14,10 +13,8 @@ def read_facts(file):
             processed_fact[key.strip()] = value.strip()
         else:
             processed_fact[fact.strip()] = True
-    print(processed_fact)
+    # print(processed_fact)
     return processed_fact
-# read_facts("facts.txt")
-
 
 def read_rules(file):
     rules=[]
@@ -28,9 +25,8 @@ def read_rules(file):
             _, item = line.split("IF")
             condition, conclusion = item.split("THEN")
             rule[condition.strip().replace('AND','and').replace('OR','or')] = conclusion.strip()
-    print(rule)
+    # print(rule)
     return rule
-# read_rules("rules.txt")
 
 def split_and(condition):
     split =[]
@@ -38,6 +34,7 @@ def split_and(condition):
         split = condition.split(' and ')
         split = [word.strip() for word in split]
     return split
+
 def split_or(condition):
     split = []
     if 'or' in condition:
@@ -53,11 +50,13 @@ def add_to_KB(item):
         knowledge_base[item] = True
     return knowledge_base
 
-
 rules = read_rules("rules.txt")
 knowledge_base = read_facts("facts.txt")
 goal = "citrus_fruit"
-def back_chaining(knowledge_base, goal) -> bool:
+
+def back_chaining(knowledge_base, goal, level) -> bool:
+    indent = "    " * level
+    print(f"{indent}Matching {goal}")
     for key, val in rules.items():
         if val == goal:
             or_split = split_or(key)
@@ -65,15 +64,15 @@ def back_chaining(knowledge_base, goal) -> bool:
             if len(or_split) > 1 and len(and_split) <= 1:
                 found = False
                 for word in or_split:
-                    print(word)
-                    print("YAYAYAYAYAY")
+                    # print(word)
+                    # print("YAYAYAYAYAY")
                     if word.split()[0] in knowledge_base:
                         knowledge_base = add_to_KB(goal)
                         found = True
                         return True
                 if found == False:
                     for word in or_split:
-                        if back_chaining(knowledge_base, word):
+                        if back_chaining(knowledge_base, word, level+1):
                             knowledge_base = add_to_KB(goal)
                             return True
             elif len(or_split) <= 1 and len(and_split) > 1:
@@ -82,12 +81,12 @@ def back_chaining(knowledge_base, goal) -> bool:
                     if word.split()[0] in knowledge_base:
                         key_copy = key_copy.replace(word.split()[0],str(knowledge_base[word.split()[0]]))
                     elif word.split()[0] not in knowledge_base:
-                        if back_chaining(knowledge_base, word):
+                        if back_chaining(knowledge_base, word, level+1):
                             knowledge_base = add_to_KB(word)
                             key_copy = key_copy.replace(word.split()[0],str(knowledge_base[word.split()[0]]))
                         else:
                             return False
-                print(key_copy)
+                # print(key_copy)
                 for item in key_copy.split():
                     if isinstance(item, str):
                         key_copy = key_copy.replace(item, "'item'")
@@ -100,82 +99,9 @@ def back_chaining(knowledge_base, goal) -> bool:
         else:
             continue
 
-
-
-
-
-            # if key in knowledge_base:
-            #     return True
-            # else:
-            #     for
-            #     back_chaining(knowledge_base, key)
-
-
-    # for rule in rules:
-    #     and_words = split_and(rule)
-    #     or_words = split_or(rule)
-    #     if len(and_words) > 0:
-    #         pass
-
-if back_chaining(knowledge_base, goal):
-    print("WOOOOW")
+if back_chaining(knowledge_base, goal, 0):
+    print("GOAL REACHED :D")
 else:
-    print("oh nOOOOOOO")
+    print("Goal couldn't be reached :(")
 
 
-            # for word in and_words:
-            #     if word in knowledge_base:
-            #         if eval(rule.replace(word,knowledge_base[word])):
-            #             print("yes")
-            #             if 'is' in rules[rule]:
-            #                 key, val = rules[rule].split('is')
-            #                 knowledge_base[key.strip()] = val.strip()
-            #             return True
-            #     else:
-
-
-
-# rule1 = "IF diameter > 2 AND diameter < 10 THEN size is medium"
-# _, rule1 = rule1.split("IF")
-# condition,conclusion = rule1.split("THEN")
-#
-# and_words =[]
-# or_words = []
-
-# if 'AND' in condition:
-#     and_words = condition.split('AND')
-# if 'OR' in condition:
-#     or_words = condition.split('OR')
-# print(and_words)
-#
-# for word in and_words:
-#     word.strip()
-#     rules = word.split()
-#     if rules[0] in knowledge_base:
-#         print("YAY")
-#     else:
-#         print("NAY")
-
-
-
-
-
-# for key, val in knowledge_base.items():
-#     for word in and_words:
-#
-#         print(word.split(' '))
-
-
-
-
-# print(condition.replace('diameter',knowledge_base['diameter']))
-# print(eval(condition.replace('diameter',knowledge_base['diameter']).replace('AND','and')))
-#
-# if eval(condition.replace('diameter',knowledge_base['diameter']).replace('AND','and')):
-#     print("yes")
-#     if 'is' in conclusion:
-#         key, val = conclusion.split('is')
-#         knowledge_base[key.strip()] = val.strip()
-# print(knowledge_base)
-
-#------------------------------------------------
